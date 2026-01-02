546bb693e1eda4ec4ea792085098f87504b4911b
HasNas03 <109939049+HasNas03@users.noreply.github.com>
2026-01-01 20:53:28 -0500

    added README.md

--- src/main/java/io/hasan/comiccatalogservice/CatalogItem.java
+++ src/main/java/io/hasan/comiccatalogservice/CatalogItem.java
@@ -3,19 +3,24 @@ package io.hasan.comiccatalogservice;
 public class CatalogItem {
 
     // attributes
+    String comicId;
     String comicName;
     String comicDesc;
     private int comicRating;
 
     // constructors
     public CatalogItem(){};
-    public CatalogItem(String comicName, String comicDesc, int comicRating) {
+    public CatalogItem(String comicId, String comicName, String comicDesc, int comicRating) {
+        this.comicId = comicId;
         this.comicName = comicName;
         this.comicDesc = comicDesc;
         this.comicRating = comicRating;
     }
 
     // getters and setters
+
+    public String getComicId() {return comicId;}
+    public void setComicId(String comicId) {this.comicId = comicId;}
     public String getComicName() {return comicName;}
     public void setComicName(String comicName) {this.comicName = comicName;}
     public String getComicDesc() {return comicDesc;}
--- src/main/java/io/hasan/comiccatalogservice/ComicCatalogController.java
+++ src/main/java/io/hasan/comiccatalogservice/ComicCatalogController.java
@@ -14,8 +14,15 @@ public class ComicCatalogController {
 
     @RequestMapping(method= RequestMethod.GET,value="/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
+        // this will return the big response, which requires:
+        // 1. userId - already have
+        // 2. comic id/name/desc - from comic-info-service
+        // 3. comic rating - from comic-rating-service
+        // list of catalog items, where each item is comic (2.) + rating (3.)
+
+        //temp
         return Collections.singletonList(
-                new CatalogItem("Secret Wars", "final event", 10));
+                new CatalogItem("7", "Secret Wars", "final event", 10));
     }
 
 }
